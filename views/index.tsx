91f892016d8325fdea831aacd22f31635c0dad97
Tim Maxwell <timmaxw@gmail.com>
2019-12-30 16:44:03 -0800

    Add better docs/intro

--- views/index.tsx
+++ views/index.tsx
@@ -4,7 +4,19 @@ export default function(props) {
   return (
     <html>
       <head><title>WikiGame</title></head>
-      <body>Under construction...</body>
+      <body>
+        <h1>WikiGame</h1>
+        <p>
+          WikiGame is a collaborative platform for making, sharing, and playing
+          online games. It's easiest to understand by example:
+        </p>
+        <p>
+          <a href="/game/22">Example Game</a>
+        </p>
+        <p>
+          Or see the <a href="/docs.html">Docs & FAQ</a>.
+        </p>
+      </body>
     </html>
   );
 }
